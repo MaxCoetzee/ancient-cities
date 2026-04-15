7d7f8a0c2afc45ada2279d5eb95d158a97b7c2f4
Max Coetzee <maxjohancoetzee@gmail.com>
2026-04-15 02:32:12 +0200

    working year slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-    import type { Settlement } from "$lib/settlement";
-    import type { PageProps } from "./$types";
     import { mount, onMount, unmount } from "svelte";
     import { MapLibre, Marker, Popup, type StyleSpecification } from "svelte-maplibre";
+    import type { PageProps } from "./$types";
 
     let { data }: PageProps = $props();
-    const SETTLEMENTS = data.settlements;
 
     const SATELLITE_STYLE: StyleSpecification = {
         version: 8,
@@ -25,6 +23,8 @@
             },
         ],
     };
+
+    let year = $state(-10000);
 </script>
 
 <svelte:head>
@@ -32,21 +32,47 @@
     <link rel="preload" href="/settlements.json" as="fetch" type="application/json" />
 </svelte:head>
 
+<div id="slider-container">
+    <p>{year}</p>
+    <input bind:value={year} id="year-slider" type="range" min="-15000" max="-1000" step="100" />
+</div>
+
 <MapLibre center={[34, 34]} zoom={3} class="map" standardControls style={SATELLITE_STYLE} projection={{ type: "globe" }}>
-    {#each SETTLEMENTS as { name, inhabitation, location, wikipediaURL }}
-        <Marker class="settlement-marker" lngLat={{ lat: location.latitude, lng: location.longitude }} anchor="top-left">
-            <a href={wikipediaURL} target="_blank">
+    {#each data.settlements as { name, inhabitation, location, wikipediaURL, pleiadesURI, description }}
+        {@const abandoned = inhabitation.end != null && inhabitation.end <= year}
+
+        {#if inhabitation.start <= year}
+            <Marker class="settlement-marker {abandoned ? 'abandoned' : ''}" lngLat={{ lat: location.latitude, lng: location.longitude }} anchor="top-left">
                 <div class="location-circle"></div>
-                <p class="label">{name}</p>
-            </a>
-            <Popup openOn="hover" offset={[6, 0]} closeButton={true}>
-                <p>start: {inhabitation.start}</p>
-            </Popup>
-        </Marker>
+                {#if !abandoned}
+                    <p class="label">{name}</p>
+                {/if}
+                <Popup openOn="click" offset={[6, 0]} closeButton={true}>
+                    <h3>{name}</h3>
+                    <p>{description}</p>
+                    <p>start: {inhabitation.start}</p>
+                    {#if inhabitation.end}
+                        <p>end: {inhabitation.end}</p>
+                    {/if}
+                    {#if wikipediaURL}
+                        <a href={wikipediaURL} target="_blank">Wikipedia</a>
+                    {/if}
+                    <a href={pleiadesURI} target="_blank">Pleiades</a>
+                </Popup>
+            </Marker>
+        {/if}
     {/each}
 </MapLibre>
 
 <style>
+    :global(.maplibregl-popup) {
+        p {
+            margin: 0;
+        }
+    }
+    :global(.settlement-marker.maplibregl-marker-covered) {
+        display: none;
+    }
     :global(body),
     :global(html) {
         width: 100%;
@@ -56,45 +82,72 @@
         background-color: black;
     }
 
+    #slider-container {
+        display: inline-flex;
+        position: fixed;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        z-index: 1;
+        background-color: white;
+
+        p {
+            margin: 0;
+        }
+
+        #year-slider {
+        }
+    }
+
     :global(.map) {
         height: 100%;
         width: 100%;
     }
 
     :global(.settlement-marker) {
+        height: 1rem;
         display: inline-flex;
         align-items: top;
-        height: 1rem;
+        cursor: pointer;
+        text-decoration: none;
 
+        .location-circle {
+            width: 0.5rem;
+            height: 0.5rem;
+            margin-right: 0.125rem;
+            border: 0.125rem solid white;
+            border-radius: 50%;
+            background-color: black;
+            box-shadow: 0 0.0625rem 0.1875rem rgb(0 0 0 / 55%);
+        }
+
+        .label {
+            color: black;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1rem;
+            font-weight: bold;
+            text-shadow:
+                -1px -1px 0 white,
+                1px -1px 0 white,
+                -1px 1px 0 white,
+                1px 1px 0 white;
+            white-space: nowrap;
+            margin: 0;
+        }
+    }
+
+    :global(.settlement-marker.abandoned) {
         a {
-            display: inline-flex;
-            align-items: top;
-            cursor: pointer;
-            text-decoration: none;
-
             .location-circle {
-                width: 0.5rem;
-                height: 0.5rem;
-                margin-right: 0.125rem;
-                border: 0.125rem solid white;
-                border-radius: 50%;
-                background: black;
-                box-shadow: 0 0.0625rem 0.1875rem rgb(0 0 0 / 55%);
+                background-color: grey;
             }
-
             .label {
-                color: black;
-                font-family: Arial, Helvetica, sans-serif;
-                font-size: 1rem;
-                font-weight: bold;
-                text-shadow:
-                    -1px -1px 0 white,
-                    1px -1px 0 white,
-                    -1px 1px 0 white,
-                    1px 1px 0 white;
-                white-space: nowrap;
-                margin: 0;
+                color: grey;
             }
         }
+    }
+
+    :global(.maplibregl-popup) {
+        max-width: 20rem;
     }
 </style>
